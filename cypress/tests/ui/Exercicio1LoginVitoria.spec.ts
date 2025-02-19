ef851971ca6362fd8f6a90d5f83770dbd8a0aff3
vitoria nascimento <vivanascimento2020@gmail.com>
2025-02-19 16:59:15 -0300

    Teste de transações bancarias

--- cypress/tests/ui/Exercicio1LoginVitoria.spec.ts
+++ cypress/tests/ui/Exercicio1LoginVitoria.spec.ts
@@ -1,6 +1,6 @@
 
 describe('Testes Login com sucesso/Falha/Cadastro de usuarios', () => {
-    it('Deve fazer login com um usuário válido', () => {
+    it.only('Deve fazer login com um usuário válido', () => {
         cy.visit("http://localhost:3000/signin")
         cy.get('#username').type("Heath93")
         cy.get('#password').type("s3cret")
---
+++ cypress/tests/ui/Exercicio2EnviarDinheiroVit.spec.ts
@@ -0,0 +1,37 @@
+describe('Teste transferência bancária', () => {
+    
+    it('Transferência menor que o saldo', () => {
+        cy.visit("http://localhost:3000/signin")
+        cy.get('#username').type("Heath93")
+        cy.get('#password').type("s3cret")
+        cy.get('[data-test="signin-submit"]').click()
+        cy.get('[data-test="nav-top-new-transaction"]').click()
+        cy.location('pathname').should('equal','/transaction/new')
+        cy.get('[data-test="user-list-item-GjWovtg2hr"] > .MuiListItemText-root > .MuiTypography-body2 > :nth-child(1) > .MuiGrid-container > :nth-child(1)').click()
+        cy.location('pathname').should('equal','/transaction/new')
+        cy.get('#amount').type(100)
+        cy.get('#transaction-create-description-input').type('For Kristian')
+        cy.get('[data-test="transaction-create-submit-payment"]').click()
+
+    });
+
+    it('Transferência maior que o saldo', () => {
+        cy.visit("http://localhost:3000/signin")
+        cy.get('#username').type("Heath93")
+        cy.get('#password').type("s3cret")
+        cy.get('[data-test="signin-submit"]').click()
+        cy.get('[data-test="nav-top-new-transaction"]').click()
+        cy.location('pathname').should('equal','/transaction/new')
+        cy.get('[data-test="user-list-item-GjWovtg2hr"] > .MuiListItemText-root > .MuiTypography-body2 > :nth-child(1) > .MuiGrid-container > :nth-child(1)').click()
+        cy.location('pathname').should('equal','/transaction/new')
+        cy.get('#amount').type(10000)
+        cy.get('#transaction-create-description-input').type('For Kristian')
+        cy.get('[data-test="transaction-create-submit-payment"]').click()
+        cy.get('.MuiAlert-message')
+
+        
+        
+
+
+    });
+})
