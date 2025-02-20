841463b33bc10744054a6385d6b54d13085d5991
vitoria nascimento <vivanascimento2020@gmail.com>
2025-02-20 20:25:25 -0300

    lista de seletores e page com dados de usuarios

--- cypress/tests/ui/Exercicio2EnviarDinheiroVit.spec.ts
+++ cypress/tests/ui/Exercicio2EnviarDinheiroVit.spec.ts
@@ -5,6 +5,7 @@ describe('Teste transferência bancária', () => {
         cy.get('#username').type("Heath93")
         cy.get('#password').type("s3cret")
         cy.get('[data-test="signin-submit"]').click()
+        cy.location('pathname')
         cy.get('[data-test="nav-top-new-transaction"]').click()
         cy.location('pathname').should('equal','/transaction/new')
         cy.get('[data-test="user-list-item-GjWovtg2hr"] > .MuiListItemText-root > .MuiTypography-body2 > :nth-child(1) > .MuiGrid-container > :nth-child(1)').click()
@@ -12,6 +13,7 @@ describe('Teste transferência bancária', () => {
         cy.get('#amount').type(100)
         cy.get('#transaction-create-description-input').type('For Kristian')
         cy.get('[data-test="transaction-create-submit-payment"]').click()
+        cy.location('pathname').should('equal','/transaction/new')
 
     });
 
